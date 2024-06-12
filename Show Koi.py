9dad101e6dc5d0e8cb975d3648fedd7d3092c3a8
jason <jasonquigg@hotmail.co.uk>
2024-06-12 07:54:50 +0100

    Ticket prices Day 3 Work

--- Show Koi.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your name? ")
-age = 26
-AmiDead = True
-
-print(f"{name}\n{age}\n{AmiDead}")
---
+++ Show_Koi.py
@@ -0,0 +1,7 @@
+age = 19
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+age_in_weeks = int(age) * 52
+weeks_in_90years = 90 * 52
+how_long_left = weeks_in_90years - age_in_weeks
+print(f"You have {how_long_left} weeks left.")
